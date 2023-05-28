8f91cd7980329c13312dea8ec7224c42fd8b0fa5
Leonardo Ferreira <56656888+leonardo-ferreiraa@users.noreply.github.com>
2023-05-28 13:04:10 -0300

    Adicionando Controller_Agenda

---
+++ Task_Done1.1/src/java/Controller/Controller_Agenda.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Agenda;
+import Model.Task_DoneDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author alunos
+ */
+@WebServlet(name = "Controller", urlPatterns = {"/Controller"})
+public class Controller_Agenda extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String op = request.getParameter("operacao");
+            String message = "Operação não realizada";
+            String pagina = "";
+            
+            if (op.equals("DELETAR")) {
+                Agenda a = new Agenda();
+                Task_DoneDAO tdao = new Task_DoneDAO();
+                try {
+                    tdao.deletarAgenda(a);
+                    message = "Deletado com sucesso!!";
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Erro ClassNotFound: " + ex.getMessage());
+                } catch (SQLException ex) {
+                    System.out.println("Erro SQL: " + ex.getMessage());
+                }
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
